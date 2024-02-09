e40940867fee0052d2a1cd11c606d722517aa849
dalalux <159000931+dalalux@users.noreply.github.com>
2024-02-09 22:50:32 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,165 +1,65 @@
-import { promises } from 'fs'
-import { join } from 'path'
-import fetch from 'node-fetch'
-import { xpRange } from '../lib/levelling.js'
-//import { plugins } from '../lib/plugins.js'
-let tags = {
-  'applications':'‎ أوامر التطبيقات‎',
-  
-  'ai':'‎ الذكاء الاصطناعي‎ أوامر',
-  'downloader':'‎ أوامر التحميلات ',
-}
-const defaultMenu = {
-  before: `السلام عليكم 👋. 
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems })  => m.reply(`
 
-┏━━ salam  *%name*
-👥 *Total user:* %totalreg 
-⏰ *Uptime:* %muptime  
-┗━━━━━━━━━━⬣
-%readmore
-  ≡ *𝗗 𝗔 𝗟 𝗔 𝗕 𝗢 𝗧 | M E N U*
-`.trimStart(),
-  header: '┏━━⊜ *_%category_* ',
-  body: '┃⋄ %cmd %isdiamond %isPremium',
-  footer: '┗━━━━━━━━⬣\n',
-  after: '*إستخدامك للبوت بشكل صحيح*\n+212646102399 ',
-}
-let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
-  try {
-    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-    let { exp, diamond, level, role } = global.db.data.users[m.sender]
-    let { min, xp, max } = xpRange(level, global.multiplier)
-    let name = await conn.getName(m.sender)
-    let d = new Date(new Date + 3600000)
-    let locale = 'ar'
-    // d.getTimeZoneOffset()
-    // Offset -420 is 18.00
-    // Offset    0 is  0.00
-    // Offset  420 is  7.00
-    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
-    let week = d.toLocaleDateString(locale, { weekday: 'long' })
-    let date = d.toLocaleDateString(locale, {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    })
-    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
-      day: 'numeric',
-      month: 'long',
-      year: 'numeric'
-    }).format(d)
-    let time = d.toLocaleTimeString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    })
-    let _uptime = process.uptime() * 1000
-    let _muptime
-    if (process.send) {
-      process.send('uptime')
-      _muptime = await new Promise(resolve => {
-        process.once('message', resolve)
-        setTimeout(resolve, 1000)
-      }) * 1000
-    }
-    let muptime = clockString(_muptime)
-    let uptime = clockString(_uptime)
-    let totalreg = Object.keys(global.db.data.users).length
-    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
-      return {
-        help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
-        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
-        prefix: 'customPrefix' in plugin,
-        diamond: plugin.diamond,
-        premium: plugin.premium,
-        enabled: !plugin.disabled,
-      }
-    })
-    for (let plugin of help)
-      if (plugin && 'tags' in plugin)
-        for (let tag of plugin.tags)
-          if (!(tag in tags) && tag) tags[tag] = tag
-    conn.menu = conn.menu ? conn.menu : {}
-    let before = conn.menu.before || defaultMenu.before
-    let header = conn.menu.header || defaultMenu.header
-    let body = conn.menu.body || defaultMenu.body
-    let footer = conn.menu.footer || defaultMenu.footer
-    let after = conn.menu.after || (conn.user.jid == conn.user.jid ? '' : `Powered by https://wa.me/${conn.user.jid.split`@`[0]}`) + defaultMenu.after
-    let _text = [
-      before,
-      ...Object.keys(tags).map(tag => {
-        return header.replace(/%category/g, tags[tag]) + '\n' + [
-          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
-            return menu.help.map(help => {
-              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
-                .replace(/%isdiamond/g, menu.diamond ? '(Ⓛ)' : '')
-                .replace(/%isPremium/g, menu.premium ? '(Ⓟ)' : '')
-                .trim()
-            }).join('\n')
-          }),
-          footer
-        ].join('\n')
-      }),
-      after
-    ].join('\n')
-    let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
-    let replace = {
-      '%': '%',
-      p: _p, uptime, muptime,
-      me: conn.getName(conn.user.jid),
-      npmname: _package.name,
-      npmdesc: _package.description,
-      version: _package.version,
-      exp: exp - min,
-      maxexp: xp,
-      totalexp: exp,
-      xp4levelup: max - exp,
-      github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
-      level, diamond, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
-      readmore: readMore
-    }
-    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+  ≡ *𝗗 𝗔 𝗟 𝗔 𝗕 𝗢 𝗧*
 
- conn.sendMessage(m.chat, {
-text: text,
-contextInfo: {
-externalAdReply: {
-title: '𝗗 𝗔 𝗟 𝗔 𝗕 𝗢 𝗧',
-body: " 𝙰𝙱𝙳𝙴𝙻𝙻𝙰𝙷 𝙱𝙾𝚄𝙰𝚉𝙰𝙾𝙾𝚄𝙸",
-thumbnailUrl:'https://telegra.ph/file/e45bc91883198c1347fa5.mp4',
-sourceUrl: 'instagram.com/__3abdollah__',
-mediaType: 1,
-renderLargerThumbnail: true
-}}}, { quoted: m})
-
-    /*conn.sendFile(m.chat, 'menu.png', text.trim(), m, null, )
-    /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff', pp, [
-      ['ꨄ︎ Apoyar', `${_p}donate`],
-      ['⏍ Info', `${_p}botinfo`],
-      ['⌬ Grupos', `${_p}gpdylux`]
-    ],m, rpl)*/
-
-  } catch (e) {
-    conn.reply(m.chat, '❎ هناك خطأ في لائحة الاوامر', m)
-    throw e
-  }
-}
-handler.help = ['menu']
-handler.tags = ['infobot']
-handler.command = ['menu','b','list'] 
-handler.register = false
-
+┏━━⊜𝗔 𝗣 𝗞 𝗗 𝗢 𝗪 
+┃⋄ . apk
+┗━━━━━━━━⬣
+┏━━⊜ 𝗜 𝗠 𝗚  
+┃⋄ .bingcreate
+┃⋄ .bimg
+┃⋄ .dalle
+┗━━━━━━━━⬣
+┏━━⊜𝗖 𝗛 𝗔 𝗧  𝗚 𝗣 𝗧
+┃⋄ .bardai
+┃⋄ .chatgbt
+┃⋄ .chawnyma
+┃⋄ .dx
+┗━━━━━━━━⬣
+┏━━⊜ 𝗠 𝗨 𝗦 𝗜 𝗖
+┃⋄ .song
+┗━━━━━━━━⬣
+┏━━⊜ 𝗔 𝗡 𝗜 𝗠 𝗘
+┃⋄ .animepic
+┃⋄ .ppcouple
+┃⋄ .tofanime
+┃⋄ .animestory
+┃⋄ .reels
+┃⋄ .resvcouple
+┃⋄ .stories
+┗━━━━━━━━⬣
+┏━━⊜ 𝗘 𝗗 𝗜 𝗧 𝗜 𝗢 𝗡  𝗜 𝗠 𝗚
+┃⋄ .blur
+┃⋄ .dehaze
+┃⋄ .recolor
+┃⋄ .hdr
+┃⋄ .remini
+┗━━━━━━━━⬣
+┏━━⊜𝗬 𝗢 𝗨 𝗧 𝗨 𝗕 𝗘
+┃⋄ .youtube
+┃⋄ .ytdoc
+┃⋄ .ytmp4
+┃⋄ .yts البحث عن رابط الفيديو 
+┗━━━━━━━━⬣
+┏━━⊜ 𝗟 𝗢 𝗚 𝗢
+┃⋄ .brandcrowd
+┃⋄ .pubg
+┗━━━━━━━━⬣
+┏━━⊜ 𝗧 𝗢 𝗢 𝗟 𝗦
+┃⋄ .bruzu
+┃⋄ .coffee
+┃⋄ .get
+┃⋄ .hd
+┃⋄ .qrcode
+┃⋄ .readqr
+┃⋄ .short
+┃⋄ .sleep
+┃⋄ .ssweb
+┃⋄ .stories
+┗━━━━━━━━⬣
+`.trim())
+handler.help = ['allmenu']
+handler.tags = ['main']
+handler.command = ['list', 'menu'] 
 
 export default handler
-
-const more = String.fromCharCode(8206)
-const readMore = more.repeat(4001)
-
-function clockString(ms) {
-  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, 'd ', h, 'h ', m, 'm '].map(v => v.toString().padStart(2, 0)).join('')
-}
